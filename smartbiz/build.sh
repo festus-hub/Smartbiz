d9205bb6e0759eaba1a6d45dd11fdb8707fda102
Festus <106164588+festus-hub@users.noreply.github.com>
2026-05-19 14:38:13 +0300

    Make Render build script path-safe

--- smartbiz/build.sh
+++ smartbiz/build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
+cd "$(dirname "$0")"
+
 python -m pip install --upgrade pip
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
